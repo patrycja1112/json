6b1ad45437cd093eb0ad5ab3cda26e0c5da6fe68
GGancarczyk <grzesiek.dzi@gmail.com>
2016-05-21 14:58:00 +0200

    structure added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
-public class Main implements Runnable {
+import jdk.nashorn.api.scripting.JSObject;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 
+public class Main {
 
-    public void sendMessage(String message) {
+    class Client implements Runnable {
+        JSObject packet;
 
-    }
+        public static final String SERVER_IP = "192.168.0.2";
+        public static final int SERVER_PORT = 5678;
 
-    @Override
-    public void run() {
+        private String mServerMessage;
+        Thread backgroundThread;
 
+        private boolean mRun = false;
+        private PrintWriter mBufferOut;
+        private BufferedReader mBufferIn;
+
+        public Client(JSObject packet) { this.packet = packet; }
+
+        public void sendMessage(String message) {
+
+        }
+
+        public void stopClient() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 
     public static void main(String[] args) {}
